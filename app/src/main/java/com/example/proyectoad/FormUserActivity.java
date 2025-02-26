e9cf3b1b16eb8e90f4b1a0b615e080f4e34b7d67
Alejandro <aaleex8842@gmail.com>
2025-02-26 19:43:24 +0100

    Clases necesarias para el formulario

--- app/src/main/java/com/example/proyectoad/FormUserActivity.java
+++ app/src/main/java/com/example/proyectoad/FormUserActivity.java
@@ -1,5 +1,6 @@
 package com.example.proyectoad;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -12,6 +13,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.proyectoad.databinding.ActivityFormUserBinding;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FormUserActivity extends AppCompatActivity {
 
@@ -31,17 +36,42 @@ public class FormUserActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String nombre = binding.etNombre.getText().toString().trim();
                 String apellidos = binding.etApellido.getText().toString().trim();
                 String piso = binding.etPiso.getText().toString().trim();
                 String telefono = binding.etTelefono.getText().toString().trim();
 
-                if (nombre.isEmpty() || apellidos.isEmpty() || piso.isEmpty() || telefono.isEmpty()) {
+                if (apellidos.isEmpty() || piso.isEmpty() || telefono.isEmpty()) {
                     Toast.makeText(FormUserActivity.this, "Rellene todos los campos", Toast.LENGTH_SHORT).show();
                 } else {
 
                     // TODO insertar los datos de usuario en su nodo
+                    ref = FirebaseDatabase.getInstance().getReference("usuarios");
 
+                    String id = getIntent().getStringExtra("id").split("@")[0].replace(".", "");
+
+                    Map<String, Object> updates = new HashMap<>();
+                    updates.put("apellidos", apellidos);
+                    updates.put("piso", piso);
+                    updates.put("telefono", telefono);
+
+                    // XXX PROBLEMA EN LA IMAGEN
+
+                    ref.child(id).updateChildren(updates).addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(FormUserActivity.this, "Formulario guardado correctamente", Toast.LENGTH_SHORT).show();
+
+                            binding.etApellido.setText("");
+                            binding.etPiso.setText("");
+                            binding.etTelefono.setText("");
+
+                            /*Intent intent = new Intent(FormUserActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish(); */
+
+                        } else {
+                            Toast.makeText(FormUserActivity.this, "Error inesperado al registrar", Toast.LENGTH_SHORT).show();
+                        }
+                    });
 
 
                 }
